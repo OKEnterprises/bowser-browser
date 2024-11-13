f5a9cc726342fb4849df255fd4c62d6d39801239
Garret O'Connor <okenerenterprises@gmail.com>
2024-11-13 11:25:13 -0500

    Scrollbar working kinda

--- browser.py
+++ browser.py
@@ -1,12 +1,15 @@
 import socket
 import ssl
 import tkinter
+from tkinter import ttk
 from datetime import datetime, timedelta
 
-cache: dict[str, tuple[str, datetime]] = {}
+type Cache = dict[str, tuple[str, datetime]]
+cache: Cache = {}
 
-WIDTH, HEIGHT = 800, 600
-
+HSTEP, VSTEP = 13, 18
+MIN_WIDTH = HSTEP * 2
+SCROLL_STEP = 100
 
 class URL:
     MAX_REDIRECTS = 10
@@ -14,11 +17,11 @@ class URL:
     def __init__(self, url: str, redirects=0):
         self.redirects = redirects
         self.url = url
-        if "data" in url:
+        if "data" in url or "about" in url:
             self.scheme, url = url.split(":", 1)
         else:
             self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https", "file", "data", "view-source"]
+        assert self.scheme in ["http", "https", "file", "data", "view-source", "about"]
 
         if self.scheme == "http":
             self.port = 80
@@ -54,7 +57,7 @@ class URL:
         if self.scheme == "https":
             ctx = ssl.create_default_context()
             s = ctx.wrap_socket(s, server_hostname=self.host)
-        
+
         request = f"GET {self.path} HTTP/1.1\r\n"
         request += f"Host: {self.host}\r\n"
         request += "Connection: keep-alive\r\n"
@@ -88,7 +91,7 @@ class URL:
             return "ERROR: Maximum redirects exceeded"
         else:
             self.redirects = 0
-        
+
         content = response.read(int(response_headers["content-length"]))
 
         if "cache-control" in response_headers and "no-store" not in response_headers["cache-control"]:
@@ -103,31 +106,95 @@ class URL:
             cache[self.url] = (content, exp_date)
 
         return content
-    
+
 class Browser:
-    def __init__(self):
+    def __init__(self, width=800, height=600):
+        self.width, self.height = width, height
         self.window = tkinter.Tk()
+        self.window.title("Bowser Browser")
         self.canvas = tkinter.Canvas(
             self.window,
-            width=WIDTH,
-            height=HEIGHT
+            width=self.width,
+            height=self.height
         )
-        self.canvas.pack()
+        self.v_scrollbar = ttk.Scrollbar(
+            self.window,
+            orient="vertical",
+            command=self.canvas.yview
+        )
+        self.canvas.configure(
+            scrollregion=self.canvas.bbox("all"),
+            yscrollcommand=self.v_scrollbar.set
+        )
+        self.v_scrollbar.pack(fill="y", side="right")
+        self.canvas.pack(fill="both", expand=True)
+        self.scroll = 0
+        self.window.protocol("WM_DELETE_WINDOW", self.window.quit)
+        self.window.bind("<Down>", self.scrolldown)
+        self.window.bind("<Up>", self.scrollup)
+        self.window.bind("<MouseWheel>", self.mousescroll)
+        self.window.bind("<Configure>", self.resize)
+
+    def draw(self) -> None:
+        self.canvas.delete("all")
+        assert self.display_list
+        for x, y, c in self.display_list:
+            if y > self.scroll + self.height: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
     def load(self, url: URL) -> None:
         if url.url in cache and cache[url.url][1] > datetime.now():
-            show(cache[url.url][0])
-            return
-        body = url.request()
-        if url.scheme == "view-source":
-            print(body, end="")
+            text = lex(cache[url.url][0])
         else:
-            show(body)
-        self.canvas.create_rectangle(10, 20, 400, 300)
-        self.canvas.create_oval(100, 100, 150, 150)
-        self.canvas.create_text(200, 150, text="Hi!")
+            body = url.request()
+            if url.scheme == "view-source":
+                self.text = body
+            else:
+                self.text = lex(body)
+        self.layout()
+        self.draw()
 
-def show(body: str) -> None:
+    def scrolldown(self, _: tkinter.Event):
+        self.scroll = min(self.scroll + SCROLL_STEP, self.canvas.winfo_height())
+        self.draw()
+
+    def scrollup(self, _: tkinter.Event):
+        self.scroll = max(self.scroll - SCROLL_STEP, 0)
+        self.draw()
+
+    def mousescroll(self, e: tkinter.Event):
+        if e.delta > 0:
+            self.scroll = max(self.scroll - e.delta, 0)
+        else:
+            self.scroll = min(self.scroll - e.delta, self.canvas.winfo_height())
+        self.draw()
+
+    def resize(self, e: tkinter.Event):
+        self.width, self.height = e.width, e.height
+        self.canvas.config(width=self.width, height=self.height)
+        self.layout()
+        self.draw()
+
+    def layout(self):
+        self.display_list: list[tuple[float, float, str]] = []
+        cursor_x, cursor_y = HSTEP, VSTEP
+        effective_width = max(self.width, MIN_WIDTH)
+        for c in self.text:
+            self.display_list.append((cursor_x, cursor_y, c))
+            if c == "\n":
+                cursor_y += 1.2 * VSTEP
+                cursor_x = HSTEP
+            elif cursor_x >= effective_width - HSTEP:
+                cursor_y += VSTEP
+                cursor_x = HSTEP
+            else:
+                cursor_x += HSTEP
+
+
+def lex(body: str) -> str:
+    text = ""
     in_tag = False
     i = 0
     while i < len(body):
@@ -138,16 +205,18 @@ def show(body: str) -> None:
             in_tag = False
             i += 1
         elif not in_tag and body[i:i+4] == "&lt;":
-            print("<", end="")
+            text += "<"
             i += 4
         elif not in_tag and body[i:i+4] == "&gt;":
-            print(">", end="")
+            text += ">"
             i += 4
         elif not in_tag:
-            print(body[i], end="")
+            text += body[i]
             i += 1
         else:
             i += 1
+    return text
+
 
 
 if __name__ == "__main__":
